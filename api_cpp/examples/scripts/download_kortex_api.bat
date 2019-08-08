a4a6652bd5a34b829291cae3bb8e4523db968b99
Alexandre Vannobel <avannobel@kinova.ca>
2019-08-08 08:36:09 -0400

    Update other files in C++ examples

---
+++ api_cpp/examples/scripts/download_kortex_api.bat
@@ -0,0 +1,34 @@
+echo off
+echo Checking if you need to download Kortex API
+set /A return_code=1
+:: Check if OS-specific folders were copied in kortex_api 
+if exist ..\kortex_api\win_mingw_x86\ (
+    echo Found the folder win_mingw_x86
+    set /A return_code=0
+) else (
+    echo Could not find folder win_mingw_x86 in folder kortex_api
+)
+
+if exist ..\kortex_api\win_msvc_x86-64\ (
+    echo Found the folder win_msvc_x86-64
+    set /A return_code=0
+) else (
+    echo Could not find folder win_msvc_x86-64 in folder kortex_api
+)
+
+:: If not, check if include and lib folders were copied in kortex_api
+if exist ..\kortex_api\include\ (
+    echo Found the folder include
+    if exist ..\kortex_api\lib\ (
+        echo Found the folder lib
+        set /A return_code=0
+    )
+)
+
+if /I "%return_code%" equ "1" (
+    echo You need to download the Kortex API
+    echo Please download it here : https://drive.google.com/open?id=1Kn6tl7sZi5sAiVHh9aW5OO9bJOWisuw7
+    echo Unzip it in api_cpp\examples\kortex_api
+)
+
+exit %return_code%
