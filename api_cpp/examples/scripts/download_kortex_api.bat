f8bf9db81c1fe2062920369fa47af2f3eb53ae1d
Felix Maisonneuve <fmaisonneuve@kinova.ca>
2021-06-15 16:27:01 -0400

    fix CMakeLists, fix link, update scripts and readme

--- api_cpp/examples/scripts/download_kortex_api.bat
+++ api_cpp/examples/scripts/download_kortex_api.bat
@@ -1,25 +1,13 @@
 echo off
 echo Checking if you need to download Kortex API
 set /A return_code=1
-:: Check if OS-specific folders were copied in kortex_api 
-if exist ..\kortex_api\win_mingw_x86\ (
-    echo Found the folder win_mingw_x86
-    set /A return_code=0
-) else (
-    echo Could not find folder win_mingw_x86 in folder kortex_api
-)
-
-if exist ..\kortex_api\win_msvc_x86-64\ (
-    echo Found the folder win_msvc_x86-64
-    set /A return_code=0
-) else (
-    echo Could not find folder win_msvc_x86-64 in folder kortex_api
-)
+:: Change forward slash to backslash for Windows directory
+set subdir=%1:/=\%
 
 :: If not, check if include and lib folders were copied in kortex_api
-if exist ..\kortex_api\include\ (
+if exist ..\kortex_api\%subdir%include\ (
     echo Found the folder include
-    if exist ..\kortex_api\lib\ (
+    if exist ..\kortex_api\%subdir%lib\ (
         echo Found the folder lib
         set /A return_code=0
     )
@@ -27,7 +15,7 @@ if exist ..\kortex_api\include\ (
 
 if /I "%return_code%" equ "1" (
     echo You need to download the Kortex API
-    echo Please download it here : https://artifactory.kinovaapps.com/ui/repos/tree/General/generic-public%2Fkortex%2FAPI%2F2.3.0
+    echo Please download it here : https://artifactory.kinovaapps.com/artifactory/generic-public/kortex/API/2.3.0/
     echo Choose the proper archive for your target
     echo Unzip it in api_cpp\examples\kortex_api
 )
